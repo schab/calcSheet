690b0592159e668a632b7d222a5c0102b554e363
hubertSwiecioch <hswiecioch@gmail.com>
2015-02-24 14:05:24 +0100

    App add

---
+++ app/src/calcSheet.java
@@ -0,0 +1,13 @@
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Hubert on 2/24/2015.
+ */
+public class calcSheet {
+
+    public static void main(String [ ] args)
+    {
+        GregorianCalendar date =new GregorianCalendar();
+       System.out.println("System Up: " + date.getTime() );
+    }
+}
