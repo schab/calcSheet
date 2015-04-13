d1edfe89b5117a73854c7765579876977fd1dec4
Sobiech <kacper.sobisz@gmail.com>
2015-04-13 18:37:08 +0200

    Changes
    Deleting old files.

--- calcApp/src/CircleDiagram.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Hubert on 3/16/2015.
- */
-public class CircleDiagram extends DiagramDrawer {
-
-
-}
--- calcApp/src/LinearDiagram.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Hubert on 3/16/2015.
- */
-public class LinearDiagram extends DiagramDrawer {
-}
