e37d6cd30e5c9af781c5c05a750bff0fb7ba05a5
Sobiech <kacper.sobisz@gmail.com>
2015-04-16 10:20:14 +0200

    heh
    heh

--- src/Gui/ToolBar.java
+++ src/Gui/ToolBar.java
@@ -1,14 +1,10 @@
 package Gui;
 
 import Functions.Images;
-import sun.swing.ImageIconUIResource;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.util.ArrayList;
 
 /**
